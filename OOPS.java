ae6f739b08776722258f7ff59fd5f82e573c0731
DharanidharanD <dharanidharan04040@gmail.com>
2026-02-03 09:26:58 +0530

    Add files via upload

---
+++ OOPS.java
@@ -0,0 +1,8 @@
+public class OOPS {
+    public static void main(String[] args){
+
+    }
+    public void count(){
+        System.out.println("HI");
+    }
+}
---
+++ perfect_number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class perfect_number {
+    public static void main(String[] args){
+        Scanner me = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = me.nextInt();
+
+    }
+}
