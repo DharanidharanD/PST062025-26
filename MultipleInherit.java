60f7cfa416b38cf3ccd4dbe783741bcf7fd571ff
Dharanidharan <dharanidharan04040@gmail.com>
2026-02-09 10:11:52 +0530

    Add files via upload

---
+++ MultipleInherit.java
@@ -0,0 +1,20 @@
+public class MultipleInherit {
+    public static void main(String[] args){
+
+    }
+}
+class Father{
+    int one(){
+        return 0;
+    }
+}
+class son extends Father{
+    int two(){
+        return 0;
+    }
+}
+class daughter extends Father{
+    int three(){
+        return 0;
+    }
+}
