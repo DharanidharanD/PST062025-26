9744f08b1c0a4ef03b4770f65c51c09dbb3c4f9c
Dharanidharan <dharanidharan04040@gmail.com>
2026-02-10 14:32:39 +0530

    Add files via upload

---
+++ A_Z.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class A_Z {
+    public void main(String[] args){
+        y();
+    }
+    public char y(){
+        for(int i=65; i<=90;i++){
+            System.out.println((char) i);
+        }
+        return 0;
+    }
+}
