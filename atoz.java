b229ff87109cc000b19a8d33b87feedbd8600fe5
Dharanidharan <dharanidharan04040@gmail.com>
2026-02-11 12:00:23 +0530

    Add files via upload

---
+++ atoz.java
@@ -0,0 +1,11 @@
+public class atoz {
+    public void main(String[] args){
+        y();
+    }
+    public char y(){
+        for(int i=97;i<=122;i++){
+            System.out.println((char) i);
+        }
+        return 0;
+    }
+}
