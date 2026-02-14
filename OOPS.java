9737ee46ffd0de5981ba799cf1fdce44b9ce830d
Dharanidharan <dharanidharan04040@gmail.com>
2026-02-14 17:17:22 +0530

    Add files via upload

--- OOPS.java
+++ OOPS.java
@@ -1,8 +1,20 @@
 public class OOPS {
     public static void main(String[] args){
-
+        Sid();
     }
-    public void count(){
-        System.out.println("HI");
+    public static void Sid(){
+        System.out.println("Sid is best");
+        Hari();
+    }
+    public static void Hari(){
+        System.out.println("Hari is ok");
+        Vish();
+    }
+    public static void Vish(){
+        System.out.println("Vihsaal is great");
+        Vichu();
+    }
+    public static void Vichu(){
+        System.out.println("Vichu is meh");
     }
 }
